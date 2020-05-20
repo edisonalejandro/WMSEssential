6f1c30b60e74697d095af792b0a9627951fde437
Edison Arriagada Muñoz <arriagada.ed@gmail.com>
2020-05-20 01:51:17 -0400

    commit

---
+++ WMSEssential/Areas/Users/Models/InputModelRegister.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+
+namespace WMSEssential.Areas.Users.Models
+{
+    public class InputModelRegister
+    {
+        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El campo apellido es obligatorio.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "El campo nid es obligatorio.")]
+        public string NID { get; set; }
+
+        
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{2})\)?[-.]?([0-9]{2})?[-.]?([0-9]{5})$", ErrorMessage = "El formato de teléfono no es válido.")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "El campo correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El correo electrónico no es una dirección de correo electrónico válido")]
+        public string Email { get; set; }
+
+        
+        [Display(Name = "Contrasena")]
+        [Required(ErrorMessage = "El campo contaseña es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El número de caracteres de {0] debe ser al menos {2}.", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
---
+++ WMSEssential/Areas/Users/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WMSEssential.Areas.Users.Models;
+
+namespace WMSEssential.Areas.Users.Pages.Acounnt
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public class InputModel: InputModelRegister
+        {
+            public IFormFile AvatarImage { get; set; }
+            [TempData]
+            public string ErrorMessage { get; set; }
+        }
+    }
+}
